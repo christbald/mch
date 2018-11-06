230e3bb57fa167b76077bd1baba22b19e83e0754
Christoph Baldow <christoph.baldow@tu-dresden.de>
2018-11-06 12:48:45 +0100

    del consts

--- R/mch.R
+++ R/mch.R
@@ -3,7 +3,7 @@
 #' Note: You need to install multicore in order to use mch:
 #' install.packages('multicore',,'http://www.rforge.net/')
 #' @export
-mch <- function(cl, cores, func, params, consts = NULL){
+mch <- function(cl, cores, func, params){
 
   if(length(cl) != length(cores))
     stop("cores vector has to be euqal to cluster size")
@@ -11,7 +11,7 @@ mch <- function(cl, cores, func, params, consts = NULL){
   if(typeof(params) != "list")
     stop("params has to be a list")
 
-  parallel::clusterExport(cl = cl, varlist = c("func", "params", "consts"), envir = environment())
+  parallel::clusterExport(cl = cl, varlist = c("func", "params"), envir = environment())
 
   norm_cores <- cores/sum(cores)
   params_count <- length(params)
@@ -28,16 +28,8 @@ mch <- function(cl, cores, func, params, consts = NULL){
   }
   cluster_params <- c(cluster_params, list(c(cores[length(cores)], ib, params_count)))
 
-  if (is.null(consts)) {
-
     return(unlist(parallel::parLapplyLB(cl = cl, X = cluster_params, fun = function(e){
       multicore::mclapply(e[2]:e[3], function(i) func(params[[i]]), mc.cores = e[1])
     }), recursive = F))
-
-  }
-
-  return(unlist(parallel::parLapplyLB(cl = cl, X = cluster_params, fun = function(e){
-    multicore::mclapply(e[2]:e[3], function(i) func(params[[i]], consts), mc.cores = e[1])
-  }), recursive = F))
 
 }
