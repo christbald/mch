8bd57c4acb6e4247c385dc4fb1ee0cb8598f5642
Christoph Baldow <christoph.baldow@tu-dresden.de>
2018-11-09 12:20:13 +0100

    change from params to mchParams, because of naming conflicts

--- R/mch.R
+++ R/mch.R
@@ -3,18 +3,18 @@
 #' Note: You need to install multicore in order to use mch:
 #' install.packages('multicore',,'http://www.rforge.net/')
 #' @export
-mch <- function(cl, cores, func, params){
+mch <- function(cl, cores, func, mchParams){
 
   if(length(cl) != length(cores))
     stop("cores vector has to be euqal to cluster size")
 
-  if(typeof(params) != "list")
-    stop("params has to be a list")
+  if(typeof(mchParams) != "list")
+    stop("mchParams has to be a list")
 
-  parallel::clusterExport(cl = cl, varlist = c("func", "params"), envir = environment())
+  parallel::clusterExport(cl = cl, varlist = c("func", "mchParams"), envir = environment())
 
   norm_cores <- cores/sum(cores)
-  params_count <- length(params)
+  params_count <- length(mchParams)
 
   job_counts <- round(params_count * norm_cores, 0)
 
@@ -28,8 +28,10 @@ mch <- function(cl, cores, func, params){
   }
   cluster_params <- c(cluster_params, list(c(cores[length(cores)], ib, params_count)))
 
+  print(cluster_params)
+
     return(unlist(parallel::parLapplyLB(cl = cl, X = cluster_params, fun = function(e){
-      multicore::mclapply(e[2]:e[3], function(i) func(params[[i]]), mc.cores = e[1])
+      multicore::mclapply(e[2]:e[3], function(i) func(mchParams[[i]]), mc.cores = e[1])
     }), recursive = F))
 
 }
